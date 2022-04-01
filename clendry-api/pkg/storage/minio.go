01628bac0c9c4cf417fb7b13b4402de4feb7508c
Artom Sopun <artomsopun@gmail.com>
2022-04-02 01:06:34 +0300

    added pkg

--- clendry-api/pkg/storage/minio.go
+++ clendry-api/pkg/storage/minio.go
@@ -1,1 +1,49 @@
 package storage
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/minio/minio-go/v7"
+)
+
+type FileStorage struct {
+	client   *minio.Client
+	bucket   string
+	endpoint string
+}
+
+func NewFileStorage(client *minio.Client, bucket, endpoint string) *FileStorage {
+	return &FileStorage{
+		client:   client,
+		bucket:   bucket,
+		endpoint: endpoint,
+	}
+}
+
+func (fs *FileStorage) GetLink(fileName string) string {
+	return fs.generateFileURL(fileName)
+}
+
+func (fs *FileStorage) Upload(ctx context.Context, input UploadInput) error {
+	opts := minio.PutObjectOptions{
+		ContentType:  input.ContentType,
+		UserMetadata: map[string]string{"x-amz-acl": "public-read"},
+	}
+
+	_, err := fs.client.PutObject(ctx, fs.bucket, input.Name, input.File, input.Size, opts)
+	return err
+}
+
+func (fs *FileStorage) Delete(ctx context.Context, object string) error {
+	opts := minio.RemoveObjectOptions{
+		GovernanceBypass: false,
+	}
+	err := fs.client.RemoveObject(ctx, fs.bucket, object, opts)
+	return err
+}
+
+// DigitalOcean Spaces URL format.
+func (fs *FileStorage) generateFileURL(filename string) string {
+	return fmt.Sprintf("http://%s/%s/%s", fs.endpoint, fs.bucket, filename)
+}
--- clendry-api/pkg/storage/storage.go
+++ clendry-api/pkg/storage/storage.go
@@ -1,1 +1,19 @@
 package storage
+
+import (
+	"context"
+	"io"
+)
+
+type UploadInput struct {
+	File        io.Reader
+	Name        string
+	Size        int64
+	ContentType string
+}
+
+type Provider interface {
+	GetLink(fileName string) string
+	Upload(ctx context.Context, input UploadInput) error
+	Delete(ctx context.Context, object string) error
+}
